abd45af78cacd4570b082120e73c982839cbf56f
gluck <gluck@192.168.1.143>
2017-01-12 20:14:44 -0500

    Created server/client

---
+++ Group5Echo/src/EchoClient.java
@@ -0,0 +1,49 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ *
+ * @author gluck
+ */
+public class EchoClient {
+
+    public static void main(String[] args) {
+        System.out.println("Group 5 Echo Client");
+        
+        try{
+            System.out.println("Awaiting connection to server...");
+            InetAddress localAddress = InetAddress.getLocalHost();
+            
+            try(Socket clientSocket = new Socket(localAddress, 6000);
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader br = new BufferedReader(new InputStreamReader(
+                    clientSocket.getInputStream()))){
+                
+                System.out.println("Connected to server");
+                Scanner scanner = new Scanner(System.in);
+                
+                while(true){
+                    System.out.println("Enter text to be echoed: ");
+                    String inputLine = scanner.nextLine();
+                    if("quit".equalsIgnoreCase(inputLine)){
+                        break;
+                    }
+                    out.println(inputLine);
+                    String response = br.readLine();
+                    System.out.println("Server response: " + response);
+                }
+                clientSocket.close();
+                }
+        }
+        catch(IOException ex){
+            ex.printStackTrace();
+        }
+    }
+    
+}
---
+++ Group5Echo/src/EchoServer.java
@@ -0,0 +1,42 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ *
+ * @author gluck
+ */
+public class EchoServer {
+
+    
+    public static void main(String[] args) {
+        String inputLine;
+        
+        System.out.println("Group 5 Echo Server");
+        
+        try(ServerSocket serverSocket = new ServerSocket(6000)) {
+            System.out.println("Awaiting connection to client...");
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Connected to client");
+            
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                clientSocket.getInputStream()));
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);  
+            
+            while((inputLine = br.readLine()) != null){
+                System.out.println("Server: " + inputLine);
+                out.println(inputLine);
+            }
+            
+        }
+        catch(IOException ex){
+        ex.printStackTrace();
+        }
+        
+    }
+}   
+
